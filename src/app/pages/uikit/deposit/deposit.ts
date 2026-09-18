09f298a5996841d6d0d9d9ece4d265a0ec64c209
qionwan11-hue <qionwan11@gmail.com>
2026-09-18 11:21:37 -0700

    first commit

--- src/app/pages/uikit/deposit/deposit.ts
+++ src/app/pages/uikit/deposit/deposit.ts
@@ -48,7 +48,7 @@ export class Deposit {
       { address: "0x3647f9d8c8961De993ce7Ad9b9445F78e527d950", name: "binancecoin", code: "bnb", scan: "assets/demo/images/scan/BNBV2.jpg" },
       { address: "rB6Cxi2XzHbpfajucYgMCnTY5xJucjKvMP", name: "ripple", code: "xrp", scan: "assets/demo/images/scan/XRP.png" },
       { address: "0x5098aA32b08A5908d2e0f4cCB407aF6910A8494f", name: "usd-coin", code: "usdc", scan: "assets/demo/images/scan/UsdcBCP20.png" },
-      { address: "4jmKBg3tbagtidG6D3TFdfXCMftaNiFMtv4HqWGFwKX6", name: "solana", code: "sol", scan: "assets/demo/images/scan/SOL.png" },
+      { address: "CdmHjBYCLjgxZU3V79x1EdpCegXZpiLfT5fSXDwPXZp9", name: "solana", code: "sol", scan: "assets/demo/images/scan/SOLV1.jpg" },
       { address: "DDhfGheWqxUhxkrENy3aWVpHecUVghH2Y9", name: "dogecoin", code: "doge", scan: "assets/demo/images/scan/DOGEV2.jpg" },
       { address: "ltc1qxnhuxntzm473hzy3uf56xf9qwk2z8e0emtlz27", name: "litecoin", code: "ltc", scan: "assets/demo/images/scan/LTC.png" }
   ]
