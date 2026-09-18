d37cd4dcc47cd39b3b98a14bd177ada15c4882cd
qionwan11-hue <qionwan11@gmail.com>
2026-09-18 11:37:02 -0700

    first commit

--- src/app/pages/uikit/deposit/deposit.ts
+++ src/app/pages/uikit/deposit/deposit.ts
@@ -46,7 +46,7 @@ export class Deposit {
       { address: "0x3647f9d8c8961De993ce7Ad9b9445F78e527d950", name: "tetherBEP20", code: "usdt(bep20)", scan: "assets/demo/images/scan/usdtBep20V1.jpg" },
       { address: "TUJmGj7Xe7So4TQ222JcJYFEqd6mGPmXkS", name: "tetherTRC20", code: "usdt(trc20)", scan: "assets/demo/images/scan/usdtTrc20V1.jpg" },
       { address: "0x3647f9d8c8961De993ce7Ad9b9445F78e527d950", name: "binancecoin", code: "bnb", scan: "assets/demo/images/scan/BNBV2.jpg" },
-      { address: "rB6Cxi2XzHbpfajucYgMCnTY5xJucjKvMP", name: "ripple", code: "xrp", scan: "assets/demo/images/scan/XRP.png" },
+      { address: "rQrH7rnbWtmKiMNic4sVj8yNwTDjNhekd", name: "ripple", code: "xrp", scan: "assets/demo/images/scan/XRPV1.jpg" },
       { address: "0x5098aA32b08A5908d2e0f4cCB407aF6910A8494f", name: "usd-coin", code: "usdc", scan: "assets/demo/images/scan/UsdcBCP20.png" },
       { address: "CdmHjBYCLjgxZU3V79x1EdpCegXZpiLfT5fSXDwPXZp9", name: "solana", code: "sol", scan: "assets/demo/images/scan/SOLV1.jpg" },
       { address: "DDhfGheWqxUhxkrENy3aWVpHecUVghH2Y9", name: "dogecoin", code: "doge", scan: "assets/demo/images/scan/DOGEV2.jpg" },
